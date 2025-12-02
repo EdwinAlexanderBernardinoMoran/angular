168a92a97981021d6ab60b1936605dffe1dcd21e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-02 12:39:32 -0600

    S3 - Tarea sobre objetos e interfaces

--- 01-bases-of-typescript/src/main.ts
+++ 01-bases-of-typescript/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 // import './topics/01-basic-types'
 // import './topics/02-objects-interface'
 // import './topics/03-functions'
-import './topics/04-functions-with-objects-arguments'
+// import './topics/04-functions-with-objects-arguments'
+import './topics/05-homework-type'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
