290c55f8ed6ff80efd5472374d472d7d853276f3
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-03 15:52:37 -0600

    S3 - Priorizar composicion sobre herencia

--- 01-bases-of-typescript/src/topics/09-clases.ts
+++ 01-bases-of-typescript/src/topics/09-clases.ts
@@ -1,9 +1,15 @@
 export class Person{
 
-    constructor(public name: string, private address: string = 'No address')
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        private address: string = 'No address'
+    )
     {}
 }
 
+// Primera forma de heredar
+/*
 export class Hero extends Person {
     constructor(
         public alterEgo: string,
@@ -13,8 +19,22 @@ export class Hero extends Person {
         super(realName, 'New yourk, USA');
     }
 }
+*/
 
-const iroman = new Hero('Iroman', 45, 'Tony Stark');
+// Segunda forma de heredar
+export class Hero{
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+
+    ){
+    }
+}
+
+const tony = new Person('Tony', 'Stark', 'New yourk, USA');
+const iroman = new Hero('Iroman', 45, 'Tony Stark', tony);
 
 console.log(iroman); // Edwin
 // console.log(iroman.address); // Error: Property 'address' is private and only accessible within class 'Person'.
