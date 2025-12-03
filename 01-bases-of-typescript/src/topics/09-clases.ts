33f1f30040f14996aeb5dc65dacd4d9911644652
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-03 15:42:17 -0600

    S3 - Extender una clase

--- 01-bases-of-typescript/src/topics/09-clases.ts
+++ 01-bases-of-typescript/src/topics/09-clases.ts
@@ -1,11 +1,20 @@
 export class Person{
 
-    constructor(public name: string, private address: string)
+    constructor(public name: string, private address: string = 'No address')
     {}
 }
 
-const iroman = new Person('Edwin Alexander Bernardino Moran', 'Av. Siempre Viva 742');
+export class Hero extends Person {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        super(realName, 'New yourk, USA');
+    }
+}
 
-console.log(iroman.name); // Edwin
-console.log(iroman.address)
+const iroman = new Hero('Iroman', 45, 'Tony Stark');
+
+console.log(iroman); // Edwin
 // console.log(iroman.address); // Error: Property 'address' is private and only accessible within class 'Person'.
