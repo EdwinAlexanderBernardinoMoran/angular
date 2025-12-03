25b09646191c17918034d6feb024ec430ab6e866
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-03 11:31:17 -0600

    S3 - Desestructuracin de Arreglos

--- 01-bases-of-typescript/src/topics/06-basic-destructuring.ts
+++ 01-bases-of-typescript/src/topics/06-basic-destructuring.ts
@@ -20,11 +20,24 @@ const audioPlayer: AudioPlayer = {
     }
 }
 
+// Destructuring the Object
 const { song, songDuration, details} = audioPlayer; // Destructuring
 const { author } = details; // Destructuring
 
 console.log('song: ', song);
 console.log('Duration: ', songDuration);
 console.log('Author: ', author);
+
+
+// Destructuring the Arrays
+const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+const [goku, vegeta, trunks] = dbz; // Destructuring
+
+console.log('Character 1: ', goku);
+console.log('Character 2: ', vegeta);
+console.log('Character 3: ', trunks);
+
+
+
 
 export {};
