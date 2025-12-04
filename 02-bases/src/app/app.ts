4be52167b83e634880f202b6c7432edbb155cb14
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-04 15:41:54 -0600

    S4 - Inicializando nuevamente el proyecto

--- 02-bases/src/app/app.ts
+++ 02-bases/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Edwin Alexander');
+  protected readonly title = signal('bases');
 }
