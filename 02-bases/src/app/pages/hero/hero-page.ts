430291e034844c6a2eaa9e91831472e62e0ae876
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-05 15:19:52 -0600

    S4 - Senales computadas - readonly signals

--- 02-bases/src/app/pages/hero/hero-page.ts
+++ 02-bases/src/app/pages/hero/hero-page.ts
@@ -1,15 +1,20 @@
-import { Component, signal } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
 
 @Component({
   templateUrl: './hero-page.html',
+  imports: [UpperCasePipe],
 })
 export class HeroPage{
   name = signal("Ironman");
   age = signal(45);
 
-  getHeroDescription(): string {
-    return `${ this.name() } - ${ this.age() } años`;
-  }
+  heroDescription = computed(() => {
+    const description = `${ this.name() } - ${ this.age() }`;
+    return description;
+  })
+
+  capitalizeName = computed(() => this.name().toUpperCase())
 
   changeHero(): void {
     this.name.set('Spiderman');
