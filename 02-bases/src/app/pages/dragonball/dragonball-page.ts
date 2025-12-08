4ce9048f1b3611539c11204375e67ec5eb252364
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-08 11:10:26 -0600

    S5 - @for - Realizar iteraciones de elementos

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  powerLevel: number;
+}
 
 @Component({
   selector: 'app-dragonball',
@@ -6,5 +12,21 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPage {
-
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      powerLevel: 9001
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      powerLevel: 8000
+    },
+    {
+      id: 3,
+      name: 'Piccolo',
+      powerLevel: 3000
+    }
+  ])
 }
