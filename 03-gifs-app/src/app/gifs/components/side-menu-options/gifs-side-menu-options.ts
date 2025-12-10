aab5b8bdb97748768bd280a6004628e18739633e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-10 16:57:40 -0600

    S6 - RouterLinks - Desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/side-menu-options/gifs-side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu-options/gifs-side-menu-options.ts
@@ -1,9 +1,26 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuOption } from '../../interfaces/menu-option';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './gifs-side-menu-options.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Most popular gifs',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search for gifs',
+      route: '/dashboard/search',
+    }
+  ]
+}
