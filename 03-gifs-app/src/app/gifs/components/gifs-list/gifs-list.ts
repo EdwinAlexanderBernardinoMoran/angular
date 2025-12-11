343fb9b0cf664f93a2be29bfe26454e1cb08ca31
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-11 11:04:23 -0600

    S6 - Tarea input (rellenos de imagenes)

---
+++ 03-gifs-app/src/app/gifs/components/gifs-list/gifs-list.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { GifsListItem } from "../gifs-list-item/gifs-list-item";
+
+@Component({
+  selector: 'gifs-list',
+  imports: [GifsListItem],
+  templateUrl: './gifs-list.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifsList {
+
+  // Recibir list
+  gifs = input.required<string[]>();
+}
