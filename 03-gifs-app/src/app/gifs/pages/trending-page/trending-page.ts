b5f3e0ddf0f255ecea44953e2d671410b225b453
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-15 16:40:22 -0600

    S7 - Preservar posicion del scroll

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,16 +1,25 @@
-import { ChangeDetectionStrategy, Component, ElementRef, inject, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifsService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
   templateUrl: './trending-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TrendingPage {
+export default class TrendingPage implements AfterViewInit{
   gifService = inject(GifsService);
+  scrollStateService = inject(ScrollStateService);
 
   // Referencia al elemento HTML del componente
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupContainer');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(event: Event){
 
@@ -21,7 +30,7 @@ export default class TrendingPage {
     const { scrollTop, scrollHeight, clientHeight } = scrollDiv;
 
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
-    console.log({isAtBottom});
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
