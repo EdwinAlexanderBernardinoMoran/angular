dd74369c07f3e30b7742a0e41b8a823ee48f3d32
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-16 19:57:39 -0600

    S9 - DaisyUI - Componentes

--- 04-country-app/src/app/country/layouts/country-layout/country-layout.ts
+++ 04-country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-country-layout',
+  selector: 'country-layout',
   imports: [RouterOutlet],
   templateUrl: './country-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
