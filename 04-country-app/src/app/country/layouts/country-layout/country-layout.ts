17725dbc0c313d4cbe79e5587da8e3fdb320c33f
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-16 21:26:36 -0600

    S9 - Menu superior e iconos

--- 04-country-app/src/app/country/layouts/country-layout/country-layout.ts
+++ 04-country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenu } from "../../components/top-menu/top-menu";
 
 @Component({
   selector: 'country-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenu],
   templateUrl: './country-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
