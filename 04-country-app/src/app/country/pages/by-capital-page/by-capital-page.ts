ff12a759693347a6afb461661e18b5cbb2283ae9
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-18 16:24:06 -0600

    S10 - Services and interfaces

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { List } from '../../components/list/list';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,9 +10,13 @@ import { List } from '../../components/list/list';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ByCapitalPage {
-  onSearch(value: string) {
-    console.log('Valor emitido');
 
-    console.log({ value });
+  countryService = inject(CountryService);
+  onSearch(query: string) {
+    console.log({ query });
+
+    this.countryService.searchByCapital(query).subscribe(countries => {
+      console.log(countries)
+    })
   }
 }
