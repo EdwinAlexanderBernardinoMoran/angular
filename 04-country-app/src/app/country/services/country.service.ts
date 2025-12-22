8566b6e1f115f1edbdaced3d05be64ee74e9995d
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-12-22 17:27:44 -0600

    S11 - Tarea - Cache de paises

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries';
-import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -12,12 +12,20 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
+
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.trim().toLowerCase();
 
+    if (this.queryCacheCapital.has(query)){
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
       map(CountryMapper.mapRestCountryItemsToCountryArray),
+      tap(countries => this.queryCacheCapital.set(query, countries)),
       catchError((error) => {
         console.error('Error fetching countries by capital:', error);
         return throwError(() => new Error(`No country with that capital was found ${query}`));
@@ -28,8 +36,13 @@ export class CountryService {
   searchByCountry(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
+    if (this.queryCacheCountry.has(query)){
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
       map(CountryMapper.mapRestCountryItemsToCountryArray),
+      tap(countries => this.queryCacheCountry.set(query, countries)),
       delay(2000),
       catchError((error) => {
         console.error('Error fetching countries by name:', error);
